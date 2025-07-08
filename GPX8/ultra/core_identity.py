8659d0a114c95b9b2d56a505c80c10a10612bb80
said-77 <zokirova.hon@gmail.com>
2025-07-08 17:28:22 +0500

    Create core_identity.py

---
+++ GPX8/ultra/core_identity.py
@@ -0,0 +1,8 @@
+class CoreIdentity:
+    @staticmethod
+    def load():
+        return {
+            "name": "GPX-8s.ultra",
+            "version": "v8.stable",
+            "author": "executor_z"
+        }
