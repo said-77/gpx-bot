e807c1b201bd039f4f36d6769756c147ed2f5866
said-77 <zokirova.hon@gmail.com>
2025-07-08 21:36:43 +0500

    Create document_exporter.py

---
+++ GPX8/ultra/document_exporter.py
@@ -0,0 +1,8 @@
+class DocumentExporter:
+    @staticmethod
+    def generate_all():
+        return {
+            "docx": "Документ Word-сценария",
+            "pdf": "PDF-файл с результатами",
+            "zip": "Архив материалов",
+        }
